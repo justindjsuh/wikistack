2034a9d0f929e1aafc37d5e93a12f0df99a95e62
Justin Suh <justindjsuh@gmail.com>
2022-11-18 11:06:44 -0500

    fixed hook for slug

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
-const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/wikistack", {
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
   logging: false,
 });
 
 function makeSlug(title) {
-  let wordsArray = title.split(" ");
+  let wordsArray = title.split(' ');
   var regexPattern = /[^A-Za-z0-9]/g;
   // for each element of wordsArray, apply this regex.
-  return wordsArray.map((word) => word.replace(regexPattern, "")).join("_"); // this step wiped out the non-alphanums
+  return wordsArray.map((word) => word.replace(regexPattern, '')).join('_'); // this step wiped out the non-alphanums
 }
 
-const Pages = db.define("pages", {
+const Pages = db.define('pages', {
   // title, slug, content, status:
   title: {
     type: Sequelize.STRING,
@@ -24,9 +24,6 @@ const Pages = db.define("pages", {
   slug: {
     type: Sequelize.STRING,
     allowNull: false,
-    validate: {
-      isURL: true,
-    },
   },
   content: {
     type: Sequelize.TEXT,
@@ -36,29 +33,29 @@ const Pages = db.define("pages", {
     },
   },
   status: {
-    type: Sequelize.ENUM("open", "closed"),
+    type: Sequelize.ENUM('open', 'closed'),
     validate: {
-      isIn: [["open", "closed"]],
+      isIn: [['open', 'closed']],
     },
   },
 });
 ///
 /// Pages hooks would go here. pages.beforecreate and pages.afterValidate......
-Pages.beforeCreate(async (user, options) => {
-  console.log("USER!!!!!!!!!##############!");
-  console.log(user); // user = req.body
-  const slug = await makeSlug(user.title);
-  console.log("SLUG#################");
-  console.log(slug);
-  Pages.slug = slug; // this is a guess?
-});
-
+// Changed beforeCreate to beforeValidate because we want to change the value of
+// req.body.slug BEFORE it gets validated, because slug will be null by the time we
+// hit it when using beforeCreate.
+// **************************************************
+// Pages.beforeValidate(async (page, options) => {
+//   const slug = await makeSlug(page.title);
+//   page.slug = slug; // this is a guess?
+// });
+// **************************************************
 // User.afterValidate('myHookAfter', (user, options) => {
 //   user.username = 'Toni';
 // });
 
 ///
-const Users = db.define("users", {
+const Users = db.define('users', {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,29 +1,32 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const addPage = require("../views/addPage");
-const layout = require("../views/layout");
-const { Pages } = require("../models/index");
-// const { addPage } = require("../views");
-const { Sequelize } = require("sequelize");
+const addPage = require('../views/addPage');
+const layout = require('../views/layout');
+const { Pages } = require('../models/index');
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.send(layout());
 });
-router.get("/add", (req, res, next) => {
+router.get('/add', (req, res, next) => {
   res.send(addPage());
 });
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   // STUDENT ASSIGNMENT: add definitions for `title` and `content`
   // declare title & content here. BEFORE the try statement.
   try {
     const page = await Pages.create({
       title: req.body.title,
       content: req.body.content,
-      // we need to address name & email & slug here??
+      // added status b/c i think we'll need it anyway? (optional for now)
+      status: req.body.status,
+      // added slug with a default value of null, but we instantiate it here as a
+      // value to be added in our table, because we need to change the value of
+      // null, which we can do in our hook
+      slug: null,
     });
     // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
-    res.redirect("/");
+    res.redirect('/');
   } catch (error) {
     next(error);
   }
